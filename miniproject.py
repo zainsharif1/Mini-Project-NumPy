d52bfbf4c4ca041d332a27d6fe41d965037179d8
Zain Sharif <zain.m.sharif@gmail.com>
2019-07-16 21:37:45 +0200

    Calculate mean and std of each of the 20 columns in array

--- miniproject.py
+++ miniproject.py
@@ -2,3 +2,9 @@ import numpy as np
 
 # Create a 1000 x 20 ndarray with random integers in the half-open interval [0, 5001).
 X = np.random.randint(0,5001,20000).reshape(1000,20)
+
+# Average of the values in each column of X
+ave_cols = X.mean(axis=0)
+
+# Standard Deviation of the values in each column of X
+std_cols = X.std(axis=0)
