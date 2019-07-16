4e522229f9fc430c74aa4711e6f94aa11bd9f7bc
Zain Sharif <zain.m.sharif@gmail.com>
2019-07-16 22:13:07 +0200

    Create Training, X Validation and Test Sets from the normalized data

--- miniproject.py
+++ miniproject.py
@@ -11,3 +11,19 @@ std_cols = X.std(axis=0)
 
 # Mean normalize X
 X_norm = (X-ave_cols)/std_cols
+
+# Create a rank 1 ndarray that contains a random permutation of the row indices of `X_norm`
+row_indices = np.random.permutation(X_norm.shape[0])
+
+train_row_indices = row_indices[:600]
+crossVal_row_indices = row_indices[600:800]
+test_row_indices=row_indices[800:]
+
+# Create a Training Set
+X_train = X_norm[train_row_indices,:]
+
+# Create a Cross Validation Set
+X_crossVal =X_norm[crossVal_row_indices,:]
+
+# Create a Test Set
+X_test =X_norm[test_row_indices,:]
